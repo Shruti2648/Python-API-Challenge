24d9c51027d4c022c94d339e3cef36f70798c3bc
Shruti Jadhav <jadhavjshruti@gmail.com>
2020-04-11 22:03:43 -0400

    Added complete Python files

--- api_keys.py
+++ api_keys.py
@@ -2,4 +2,7 @@
 weather_api_key = "28e9ca97bacd746a9c9247b694afa7c4"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+
+g_key = "AIzaSyAebQoWOvlsFOxcG0CcJeFPjxvMQaZeJxk"
+
+# g_key = "AIzaSyDkuiYiXxbu9jclBPgRCB29loWsZu6at78"
